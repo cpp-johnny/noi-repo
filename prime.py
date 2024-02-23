b21d06f3733099de49b8f022804c0b83d226ba59
boi <119715263+cpp-johnny@users.noreply.github.com>
2024-02-23 20:11:29 +0800

    Add files via upload

---
+++ prime.py
@@ -0,0 +1,26 @@
+import math
+
+def is_prime(n):
+    if n <= 1:
+        return False
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+    for i in range(3, int(math.sqrt(n)) + 1, 2):
+        if n % i == 0:
+            return False
+    return True
+
+def main():
+    number = int(input())
+    if is_prime(number):
+        print("Prime")
+    else:
+        print("Not Prime")
+
+'''
+if __name__ == "__main__":
+    main()
+'''
+main()
